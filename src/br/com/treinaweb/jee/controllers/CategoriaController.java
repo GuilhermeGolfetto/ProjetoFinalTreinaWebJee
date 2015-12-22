9ccd5f6a3c55dc02383c5fcf9b9a6da4e5284fb4
Guilherme Golfetto <golfetto.guilherme@gmail.com>
2015-12-22 15:50:11 -0200

    Projeto Final JEE

---
+++ src/br/com/treinaweb/jee/controllers/CategoriaController.java
@@ -0,0 +1,67 @@
+package br.com.treinaweb.jee.controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.treinaweb.jee.dao.CategoriaDao;
+import br.com.treinaweb.jee.models.Categoria;
+
+/**
+ * Servlet implementation class CategoriaController
+ */
+@WebServlet(name = "CategoriaController", urlPatterns = { "/categorias" })
+public class CategoriaController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CategoriaController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<Categoria> categorias = CategoriaDao.todos();
+		request.getSession().setAttribute("categorias", categorias);
+		response.sendRedirect("listarCategorias.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
